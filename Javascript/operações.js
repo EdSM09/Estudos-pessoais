c5b1ce3a2da0bfab5ac33e0e8a7c236735fa8a95
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-05-31 17:24:52 -0300

    básicoJavascript

---
+++ Javascript/operações.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 20;
+console.log(x + y);
+console.log(x - y);
+console.log(y - x);
+console.log(x * y);
+console.log(x ** y);
+console.log(y ** x);
+console.log(y / x);
+console.log(x / y);
+console.log(x % y);
+console.log(y % x);
+
