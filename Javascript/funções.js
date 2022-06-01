c7939e5557528acea959445b872caa0be4ea466d
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-01 09:49:03 -0300

    funções

---
+++ Javascript/funções.js
@@ -0,0 +1,16 @@
+// uma função pode ser declarada com function verboSubstantivo(argumento) {}
+// exemplo a função pickRandom nesse repositório
+//exemplo:
+// função que toma como argumento numbers em um array e retorna a soma deles
+let valorSoma = soma([4, 5]);
+let valorSubtração = subtração([45, 562])
+console.log(valorSoma);
+console.log(valorSubtração);
+function soma(argument ){
+    return argument[0] + argument[1];
+}
+function subtração(argument) {
+    return argument[0] - argument[1];
+}
+
+    
