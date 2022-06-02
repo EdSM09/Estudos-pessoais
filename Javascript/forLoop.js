27fa3254a4d1f8f01f0039e299590b65c00560e7
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-02 00:09:49 -0300

    loops

---
+++ Javascript/forLoop.js
@@ -0,0 +1,12 @@
+// um loop servem para encurtar tarefas repetitivas 
+// basicamente, se inicia um loop com um contador, uma condição de saida, e um interador
+// exempplo:
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+for(var i = 0; i < num.length; i++) {
+    console.log(num[i] + num[i + 1]);
+}
+
+
+
