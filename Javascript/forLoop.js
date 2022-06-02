68eb64bb0c917d20160f77400dda2c35171bd973
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-02 00:25:42 -0300

    loops

--- Javascript/forLoop.js
+++ Javascript/forLoop.js
@@ -2,11 +2,13 @@
 // basicamente, se inicia um loop com um contador, uma condição de saida, e um interador
 // exempplo:
 
-let num = [1,2,3,4,5,6,7,8,9,10];
+// definir
+let num = [0,1,2,3,4,5,6,7,8,9,10];
 
-for(var i = 0; i < num.length; i++) {
+for(var i = 0; i < num.length - 1; i++) {
     console.log(num[i] + num[i + 1]);
 }
+
 
 
 
