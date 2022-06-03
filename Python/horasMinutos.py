9588f0adbc41fd3fabed031d4b08b6dcc7d3a7a8
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 15:58:41 -0300

    comentários

--- Python/horasMinutos.py
+++ Python/horasMinutos.py
@@ -3,8 +3,10 @@
 # definir uma função 
 # printar o resultado final em minutos
 
+# função para converter horas em minutos
 def converter(hrs):
     return hrs * 60
 
+# perguntar número de horas 
 hrs = int(input("Horas: "))
-print(converter(hrs))
+print(converter(hrs)) # imprimir número de minutos resultantes
