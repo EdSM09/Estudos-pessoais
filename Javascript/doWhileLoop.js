4a11743aa1872610519b9467c50bdd8ec097b9c7
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 09:36:00 -0300

    primeira tentativa

---
+++ Javascript/doWhileLoop.js
@@ -0,0 +1,18 @@
+// A declaração do...while cria um laço que executa uma declaração até que o teste da condição for falsa (false). A condição é avaliada depois que o bloco de código é executado, resultando que uma declaração seja executada pelo menos uma vez.
+// exemplo:
+
+var resultado = doWhile([1,2,3,4,5,6,7,8,9,10]);
+console.log(resultado)
+
+
+
+
+
+function doWhile(argument){
+    let i = 0;
+    do{
+        console.log(argument[i] + argument[i + 1]);
+        i++;
+    }
+    while( i < argument.length)
+}
