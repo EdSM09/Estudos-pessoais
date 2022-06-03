fc44f6368a548c08998568a8a2c9ca2e9694abe7
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 11:53:17 -0300

    FizzBuzz

--- Javascript/fizzbuzz.js
+++ Javascript/fizzbuzz.js
@@ -11,16 +11,23 @@
 //      imprima o número 
 
 for(var i = 0; i < 101; i++){
+    // para i menor que 101 sera executado
+
+    // se o numero i for tanto divisivel por 5 e 3
     if( i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
+        console.log("FizzBuzz");  // imprima FizzBuzz
     }
+    
+    // se o número i for divisível por 3
     else if(i % 3 == 0){
-        console.log("Buzz");
+        console.log("Buzz");  // imprima Buzz
     }  
+
+    // se o número i for divisível por 
     else if(i % 5 === 0){
-        console.log("Fizz")
+        console.log("Fizz");  //imprima Fizz
     }
     else{
-        console.log(i)
+        console.log(i);  //imprima o número i
     }
 }
