8bb74c67d93350b20b467d6bc283471e9760d2aa
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 15:34:41 -0300

    Função Converter minutos segundos

---
+++ minutosSegundos.py
@@ -0,0 +1,11 @@
+# Função que transforma tempo em minutos em segundos
+# COnseguir input do tempo em segundos
+# definir uma função 
+# printar o resultado final em segundos 
+
+def converter( min):
+    return min * 60
+
+min = int(input("Minutos: "))
+print(converter(min))
+
