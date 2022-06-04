5c30b33f1b167fa7d4b6cf46780414e903f6dc16
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 22:58:02 -0300

    ordemSemComentários

--- Python/ordem.py
+++ Python/ordem.py
@@ -1,6 +1,12 @@
 lista = [1,5,3,8,9,40,50]
-tipo = input("Tipo")
+tipo = input("Tipo: ")
 print(lista)
 
-lista.sort()
-print(lista)
+if tipo.upper()== "ASC":
+    lista.sort()
+    print(lista)
+elif tipo.upper()== "DESC":
+    lista.sort(reverse = True)  
+    print(lista)
+elif tipo.upper()== "NONE":
+    print(lista)
