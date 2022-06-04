db1ad1cf9a79582e96dcef835c90900f0d502f17
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 22:47:56 -0300

    segunda tentativa

--- Python/ordem.py
+++ Python/ordem.py
@@ -1,32 +1,6 @@
-# função que ordena uma sequência de números 
-# ela toma dois argumentos a lista, e o tipo de ordem descendente DESC e ascendente ASC
+lista = [1,5,3,8,9,40,50]
+tipo = input("Tipo")
+print(lista)
 
-def ordem(lista, tipo):
-    i = 0
-    if tipo.upper() == "ASC":
-        for i in enumerate(lista):
-            for j in range(i + 1, len(lista)):
-                if lista[i] >= lista[j]:
-                    t = lista[i]
-                    lista[j] = lista[i]
-                    lista[i] = t
-                    print(lista)
-    elif tipo.upper() == "DESC": 
-        for i in enumerate(lista):
-            for j in range(i + 1, len(lista)):
-                t = lista[i]
-                lista[j] = lista[i]
-                lista[i] = t
-                print(lista)
-            
-
-
-lista= [8,5,9,10,45,63]
-tipo = input("Tipo de ordem: ")
-
-print(ordem(lista, tipo))
-
-
-
-
-
+lista.sort()
+print(lista)
