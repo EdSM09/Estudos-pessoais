0b06f177b7707d7f866462e420dc91fc5af8985f
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-04 12:37:08 -0300

    comentários

--- Python/vogais.py
+++ Python/vogais.py
@@ -1,14 +1,18 @@
 # Programa que conta o número de vogais dento de um texto
 
+
+# função que toma como argumento a variável string e retorna um valor int que representa a quantidade de vogais no texto
 def contarVogais(string):
-    string = string.lower()
-    vogais = 'a,e,i,o,u'
-    resultado = 0
-    for i in vogais:
-        resultado += string.count(i)
-    return resultado
     
+    string = string.lower() # formatar o texpo para caracteres minusculos
+    vogais = 'a,e,i,o,u' #definir as vogais
+    resultado = 0 # um contador que vai armazenar o resultado
+    for i in vogais: # para todos os valores de i em vogais
+        resultado += string.count(i) # some o resultado com a quantidade de caracteres i equivalentes em string e vogais
+    return resultado # devolver o número de vogais 
     
-string = input("texto: ")             
+ # pegar o texto do usuário   
+string = input("texto: ")     
+# imprimir a quantidade de vogais do texto obtido        
 print(contarVogais(string))
 
