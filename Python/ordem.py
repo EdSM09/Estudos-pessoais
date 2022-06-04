1320245b96fa41488320c3f55cdf259a202c049e
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-03 23:06:07 -0300

    Comentários

--- Python/ordem.py
+++ Python/ordem.py
@@ -1,12 +1,21 @@
+# como ordenar listas
+# definir a lista como um array de valores int
 lista = [1,5,3,8,9,40,50]
+# pedir o tipo de ordem, crescente ou decrescente 
 tipo = input("Tipo: ")
+# imprimir lista original
 print(lista)
 
+# Se o tipo for ASC organize a lista de forma crescente
 if tipo.upper()== "ASC":
     lista.sort()
     print(lista)
+    
+# Se o tipo for DESC organize a lista de forma decrescente
 elif tipo.upper()== "DESC":
     lista.sort(reverse = True)  
     print(lista)
+    
+# Se o tipo for NONE imprima lista como esta 
 elif tipo.upper()== "NONE":
     print(lista)
