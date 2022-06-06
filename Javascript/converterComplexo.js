e56d532d51d1f12d16b1a68956a584ff8c91cf14
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 14:44:54 -0300

    comentarios + ajustes

--- Javascript/converterComplexo.js
+++ Javascript/converterComplexo.js
@@ -1,41 +1,50 @@
 // versão mais complexa do converter
-// ela pode tomar como argumento dias, horas, minutos como argumento
 
+
+// definir os valores a serem transformados
 let dias = 5;
 let horas = 2;
 let minutos = 3;
 
 
+// função para converter dias para horas, minutos e segundos
 function converterDias(argument){
      let horas = argument * 24;
      let minutos = horas * 60;
      let segundos = minutos * 60;
      console.log("Equivalem a: "+ horas + " horas, " + minutos + " minutos, e "+ segundos + " segundos.")
 }    
+
+// função para converter horas em minutos e segundos
 function converterHoras(argument){
     let minutos = argument * 60;
     let segundos = minutos * 60;
     console.log("Equivalem a: " + minutos + " minutos e " + segundos + " segundos.")
 }
+
+// função para converter minutos em segundos
 function converterMinutos(argument){
     let segundos = argument * 60;
     console.log("Equivalem a: "+segundos + " segundos.");
 }
 
-console.log(" ")
-console.log("Conversão dos dias: ")
+
+// condições para execultar as funções
+
 if(dias > 0);{
+    console.log(" ")
+    console.log("Conversão dos dias: ")
     converterDias(dias)
 }
 
-console.log(" ")
-console.log("Conversão das horas:")
 if(horas > 0);{
+    console.log(" ")
+    console.log("Conversão das horas:")
     converterHoras(horas)
 }
-console.log(" ")
-console.log("Conversão dos minutos")
 if(minutos > 0);{
+    console.log(" ")
+    console.log("Conversão dos minutos")
     converterMinutos(minutos)
 }
 
