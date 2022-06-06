5e4b6b0a798ee0b68015ac84adeea5a4a86cc7e4
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 12:54:20 -0300

    declaração da função

--- Javascript/converterHorarios.js
+++ Javascript/converterHorarios.js
@@ -1,1 +1,11 @@
+// definir quantidade de horas 
 let horas = 4;
+
+
+// usar a variável horas para realizar as operações
+function converter(argument){
+    let minutos = argument * 60;
+    let segundos = minutos * 60;
+    console.log("equivale a " + minutos + " minutos");
+    console.log("equivale a " + segundos + " segundos")
+}   
