6730941601b89509223c8764c9472f30b31f1c7b
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 13:33:29 -0300

    if

--- Javascript/converterComplexo.js
+++ Javascript/converterComplexo.js
@@ -10,12 +10,17 @@ function converter(argument){
      horas = dias * 24;
      minutos = argument * 60;
      segundos = minutos * 60;
+     console.log("equivale a " + horas + " horas");
      console.log("equivale a " + minutos + " minutos");
- 
-     console.log("equivale a " + minutos + " minutos");
-     console.log("equivale a " + segundos + " segundos")
+     console.log("equivale a " + segundos + " segundos");
 }
 
-if(dias >= 1){
-    console.log(converter(dias))
+if(dias > 0);{
+    converter(dias);
+}
+if (horas > 0 );{
+    converter(horas);
+}
+if(minutos > 0 );{
+    converter(minutos);
 }
