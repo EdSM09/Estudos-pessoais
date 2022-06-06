91d08b3fc78913436f7bcf3763d380ab2d2e8715
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 13:24:15 -0300

    alteração na função original

--- Javascript/converterComplexo.js
+++ Javascript/converterComplexo.js
@@ -1,5 +1,21 @@
 // versão mais complexa do converter
 // ela pode tomar como argumento dias, horas, minutos como argumento
 
-dias = window.prompt("Número de dias: ")
-console.log(dias)
+let dias = 1
+let horas = 2
+let minutos = 45
+
+
+function converter(argument){
+     horas = dias * 24;
+     minutos = argument * 60;
+     segundos = minutos * 60;
+     console.log("equivale a " + minutos + " minutos");
+ 
+     console.log("equivale a " + minutos + " minutos");
+     console.log("equivale a " + segundos + " segundos")
+}
+
+if(dias >= 1){
+    console.log(converter(dias))
+}
