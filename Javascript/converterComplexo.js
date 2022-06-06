eb55cd859d43ca2c7dae0adef1450bd12d48e778
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 13:37:13 -0300

    tentativa de concertar bug

--- Javascript/converterComplexo.js
+++ Javascript/converterComplexo.js
@@ -2,25 +2,22 @@
 // ela pode tomar como argumento dias, horas, minutos como argumento
 
 let dias = 1
-let horas = 2
-let minutos = 45
+let horas = 0
+let minutos = 0
 
 
 function converter(argument){
      horas = dias * 24;
      minutos = argument * 60;
      segundos = minutos * 60;
-     console.log("equivale a " + horas + " horas");
-     console.log("equivale a " + minutos + " minutos");
-     console.log("equivale a " + segundos + " segundos");
+    //  console.log("equivale a " + horas + " horas");
+    //  console.log("equivale a " + minutos + " minutos");
+    //  console.log("equivale a " + segundos + " segundos");
 }
 
 if(dias > 0);{
-    converter(dias);
-}
+    console.log("equivale a " + converter(dias) + " horas");}
 if (horas > 0 );{
-    converter(horas);
-}
+    console.log("equivale a " + converter(horas) + " minutos");}
 if(minutos > 0 );{
-    converter(minutos);
-}
+    console.log("equivale a " + conveter(minutos) + " segundos");}
