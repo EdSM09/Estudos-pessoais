583bd8b70709651cde352c6db756ec92eb5aa41d
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-07 17:27:12 -0300

    função contar letras

---
+++ Javascript/contarLetras.js
@@ -0,0 +1,18 @@
+// Função que conta todos caracteres alfabeticos
+function contarLetras(argument){
+    
+    if(argument === string){
+        argument = argument.toLowerCase();
+        let contador = 0
+        for(var i = 0; i < argument.length; i++){
+            if( argument[i].isAlpha()){
+                contador+= 1;
+            }
+        }
+    }
+
+}
+
+
+
+texto = ""
