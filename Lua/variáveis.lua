f5d4369de6f70ac611293e67811df0307d7bef77
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 23:41:29 -0300

    variáveis1

---
+++ Lua/variáveis.lua
@@ -0,0 +1,6 @@
+-- Lua consegue interpretar o tipo da variável somente pelo valor atribuído a ela
+-- O nome de uma variável pode começar com um número
+-- Por exemplo:
+
+nome = "Marcus"
+print(nome)
