68ff8b81aa0fb59cdc35d5ff3fb71e39abe78102
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-07 09:49:53 -0300

    operadores

--- Lua/operadores.lua
+++ Lua/operadores.lua
@@ -1,5 +1,16 @@
 --[[ Os operadores aritmáticos básicos são os mesmos de outras linguagens + - * / %
      Outras operações podem ser acessadas usando as funções Math: 
      flor, ceil, max, min, exp, sqrt, sen, cos, tan, pow, pi etc
-     
 ]]
+
+Num = 5
+Num2 = 10
+Sum = Num + Num2
+io.write(Sum, "\n")
+
+print(math.pi)
+
+print(math.exp(1))
+
+print(math.random(0, 1000))
+
