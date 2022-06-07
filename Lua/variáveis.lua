1853d27e411444c97a283769b9451aefc69a1085
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-06 23:51:21 -0300

    comentários

--- Lua/variáveis.lua
+++ Lua/variáveis.lua
@@ -2,10 +2,20 @@
 -- O nome de uma variável pode começar com um número
 -- Por exemplo:
 
+
+--[[
+    Vamos primeiro definir uma variável Nome
+    ela armazena um valor "Marcus" que a linguagem interpreta como uma string
+]]
 Nome = "Marcus"
 print(Nome)
-print(type(Nome))
+print(type(Nome)) -- lembre-se disso
 
+
+--[[
+    Quando o valor da variável anterior é alterado para o num 14
+    o tipo da variável é mudado tambem de string --> num
+]]
 Nome = 14
 print(Nome)
-print(type(Nome))
+print(type(Nome)) 
