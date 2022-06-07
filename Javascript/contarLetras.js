b28fddde33cdb6905ca79dafdd2e13ce0345411a
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-07 17:44:47 -0300

    mais ajustes

--- Javascript/contarLetras.js
+++ Javascript/contarLetras.js
@@ -1,20 +1,20 @@
 // Função que conta todos caracteres alfabeticos
+
+texto = "oi tudo bem"
+
 function contarLetras(argument){
     
-    if(typeof argument === 'string'){
-        argument = argument.toLowerCase();
-        let contador = 0
-        for(var i = 0; i < argument.length; i++){
-            if(argument[i] == ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']){
-                contador+= 1;
-                return contador;
-            }
+    argument = argument.toLowerCase();
+    let contador = 0
+    for(var i = 0; i < argument.length; i++){
+        if(argument[i] == ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']){
+            contador+= 1;
+            return contador;
         }
     }
-
 }
 
 
 
-texto = "oi"
+
 console.log(contarLetras(texto))
