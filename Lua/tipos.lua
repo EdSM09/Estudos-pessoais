2e5363f9ad95d38743a871268bc061527068455b
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-07 00:07:46 -0300

    pontos de precisão

---
+++ Lua/tipos.lua
@@ -0,0 +1,8 @@
+-- Vou somente explorar alguns tipos na linguagem
+-- lembrete: Lua não faz distinção entre floats ints longs etc todos eles são do tipo num
+
+
+-- lua tem uma boa precisãoo com relação a numeros do tipo float
+Var1 = 1.999999999999 + 0.0000000000001
+io.write(Var1)
+io.write(type(Var1))
