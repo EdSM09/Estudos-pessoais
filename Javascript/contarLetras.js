6bc3e15f9c538728461a4b58d975bc742a5bfa73
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-07 17:39:36 -0300

    ajustes

--- Javascript/contarLetras.js
+++ Javascript/contarLetras.js
@@ -7,6 +7,7 @@ function contarLetras(argument){
         for(var i = 0; i < argument.length; i++){
             if(argument[i] == ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']){
                 contador+= 1;
+                return contador;
             }
         }
     }
