bbb4415925f943cd1853e85d3fc362b7f9030ad9
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-07 08:16:41 -0300

    explicações

---
+++ Lua/operadores.lua
@@ -0,0 +1,5 @@
+--[[ Os operadores aritmáticos básicos são os mesmos de outras linguagens + - * / %
+     Outras operações podem ser acessadas usando as funções Math: 
+     flor, ceil, max, min, exp, sqrt, sen, cos, tan, pow, pi etc
+     
+]]
