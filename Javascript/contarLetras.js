62cde2f4b4789ce46df3ca44c22b38f7c3c7c34a
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-12 16:51:31 -0300

    programa consegue contar

--- Javascript/contarLetras.js
+++ Javascript/contarLetras.js
@@ -1,23 +1,21 @@
 // Função que conta todos caracteres alfabeticos
 
-let texto = ["oi eu sou o goku"]
-function isLetter(str) {
-    return str.length === 1 && str.match(/[a-z]/i);
-  }
+let texto = " a d v"
   
 
-  // A função está devolvendo um número  porém não o número certo
-
+// função está contando letras e espços
 function contarLetras(argument){
     let n = 0
-    if( typeof(argument) === 'string' )
-        for(var i = 0; i < argument.length; i++){
-            if(isLetter(argument[i])){
-                n++
-                return n
-                }
-            }
+    console.log(typeof(argument))
+
+    if( typeof(argument) === 'string' ){
+        n = argument.length
+        return n
+    }
+
 }
 
-console.log(contarLetras(texto))
+let comp = contarLetras(texto)
+
+console.log(comp)
 
