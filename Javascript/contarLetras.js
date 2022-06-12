38970208de152a01d4d378c57d2ea2b4d8acbfc7
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-12 17:43:18 -0300

    versão final

--- Javascript/contarLetras.js
+++ Javascript/contarLetras.js
@@ -1,7 +1,7 @@
 // Função que conta todos caracteres alfabeticos
 
 
-let texto = "teste a"
+let texto = "a b c d"
   
 function contarLetras(argument){
     let n = 0 // variável local que armazena o comprimento total da string
@@ -10,12 +10,14 @@ function contarLetras(argument){
     for(var i= 0; i < argument.length; i++) // para todo o comprimento do argumento
         if( typeof(argument) === 'string' ){ // se o argumento for uma string execute o seguinte
             n = argument.length // incrementar um valor a essa variável
-            if (argument[i] === " "){ // dar consideração aos espaços
-                s++ // incrementar ao valor 
+            if (argument[i] == " "){ // dar consideração aos espaços
+                s = s + 1 // incrementar ao valor 
         } 
-        return n - s // devolver o valor total de caracteres dentro da string
     }
+        return n - s // devolver o valor total de caracteres dentro da string
 }
+
+// se texto for uma string imprimir o número de caracteres
 if (typeof(texto) === 'string'){
     console.log(contarLetras(texto))
 }
