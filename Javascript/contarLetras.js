c71db4ee1b05af4176611b5170604cda3bbfc722
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-12 16:53:22 -0300

    funciona

--- Javascript/contarLetras.js
+++ Javascript/contarLetras.js
@@ -1,11 +1,10 @@
 // Função que conta todos caracteres alfabeticos
 
 
-// verificar se a função conta caracteres acentuados
 let texto = "éôÃ"
   
 
-// função está contando letras e espços
+// função está contando letras e espços 
 function contarLetras(argument){
     let n = 0
     console.log(typeof(argument))
