e3818c82937ae593adf3fb476bb308cb2a98e698
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-12 22:25:35 -0300

    bugFunção

--- Javascript/mario.js
+++ Javascript/mario.js
@@ -2,5 +2,20 @@
 // uma adaptação de um problem set do Curso CS50
 
 // pegar uma int para definir a altura da piramide
+let altura = 5
 
+// fazer uma função imprimirPiramide
 // repetir pela colunas e linhas 
+ function imprimirPiramide(argument){
+    for(var i = 0; i < argument; i++){
+        for(var j = argument - 1; j > i; j --){
+            console.log(" ")
+        }
+        for(var j = 0; j <= i; j++){
+            console.log("#")
+        }
+        console.log("\n")
+    }
+ }
+
+console.log(imprimirPiramide(altura))
