48bb900304b06b7d32343f876ee4ea82fc90334e
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-12 21:55:58 -0300

    pseudoCode

---
+++ Javascript/mario.js
@@ -0,0 +1,6 @@
+// imprimir uma piramide igual ao do mario
+// uma adaptação de um problem set do Curso CS50
+
+// pegar uma int para definir a altura da piramide
+
+// repetir pela colunas e linhas 
