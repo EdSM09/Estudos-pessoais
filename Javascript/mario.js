fa1444a6b81ac44ee4d046d69f38c79af8e94af1
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-12 22:37:25 -0300

    nova tentativa

--- Javascript/mario.js
+++ Javascript/mario.js
@@ -6,16 +6,21 @@ let altura = 5
 
 // fazer uma função imprimirPiramide
 // repetir pela colunas e linhas 
- function imprimirPiramide(argument){
-    for(var i = 0; i < argument; i++){
-        for(var j = argument - 1; j > i; j --){
-            console.log(" ")
-        }
-        for(var j = 0; j <= i; j++){
-            console.log("#")
-        }
-        console.log("\n")
-    }
- }
+
+//  function imprimirPiramide(argument){
+//     for(var i = 0; i < argument; i++){
+//         for(var j = argument - 1; j > i; j --){
+//             console.log(" ")
+//         }
+//         for(var j = 0; j <= i; j++){
+//             console.log("#")
+//         }
+//         console.log("\n")
+//     }
+//  }
+
+function imprimirPiramide(argument){
+    
+}
 
 console.log(imprimirPiramide(altura))
