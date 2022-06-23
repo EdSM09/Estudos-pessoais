02e47821b71904c139595c2824769dac43b6f6f1
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-23 10:08:30 -0300

    comentários

--- Python/numPrimos.py
+++ Python/numPrimos.py
@@ -1,13 +1,16 @@
-# Programa para verificar se um numero inteiro é primo
+# Programa para verificar se um numero natural inteiro é primo
 
 
 #pedir por um numero inteiro
 num = int(input("Número: "))
 
+# definir uma função que checa se um número natural inteiro é ou não é primo
 def numPrimo(num):
+    # se o número dado for divisível por ele mesmo ou 1
     if( num / num == 1) and (num / 1 != float):
-        print("É um número primo")
-    else:
-        print("Não é um número primo")
+        print("É um número primo")      # imprima "É um número primo"
+    else:   # senão
+        print("Não é um número primo")  #imprima "Não é um número primo"
 
+# passar o valor num como argumento
 numPrimo(num)
