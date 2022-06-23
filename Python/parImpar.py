9b9c424f1cea5e10973f841bf3c026aa9b4e7d18
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-23 10:20:18 -0300

    função#1

--- Python/parImpar.py
+++ Python/parImpar.py
@@ -3,4 +3,12 @@
 
 # criar uma lista
 num = list(range(0, 1000))
-print(num)
+# print(num)
+
+# definir função que para o compriento da lista cheque a condição par ou impar
+def imparPar(num):
+    for(int i = 0; i < len(num); i++):
+        if num[i] % 2 == 0:
+            print("Par!")
+        else:
+            print("Impar!")
