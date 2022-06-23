9cbd23d6fc70ed6657add1fb85a356c80ddc4cbe
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-23 10:30:00 -0300

    forLoop bugado

--- Python/parImpar.py
+++ Python/parImpar.py
@@ -7,10 +7,10 @@ num = list(range(0, 1000))
 
 # definir função que para o compriento da lista cheque a condição par ou impar
 def imparPar(num):
-    for(int i = 0, i < len(num), i++):
+    for i < len(num):
         if num[i] % 2 == 0:
             print("Par!")
         else:
             print("Impar!")
-            
+        i++    
 imparPar(num)
