312b71f97e4cfa20c8e19d3876ab9302d70378dc
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-23 09:59:08 -0300

    função #1

--- Python/numPrimos.py
+++ Python/numPrimos.py
@@ -1,3 +1,10 @@
 # Programa para verificar se um numero inteiro é primo
 
+
+#pedir por um numero inteiro
 num = int(input("Número: "))
+
+def numPrimo(num):
+    if( num / num === 1) and (num / 1 !== float):
+        print("É um número primo")
+
