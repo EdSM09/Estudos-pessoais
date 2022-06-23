a1d4dffd5e57eb13e5c00f75fb7f869591434074
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-23 10:27:19 -0300

    syntax

--- Python/parImpar.py
+++ Python/parImpar.py
@@ -7,7 +7,7 @@ num = list(range(0, 1000))
 
 # definir função que para o compriento da lista cheque a condição par ou impar
 def imparPar(num):
-    for(int i = 0; i < len(num); i++):
+    for(int i = 0, i < len(num), i++):
         if num[i] % 2 == 0:
             print("Par!")
         else:
