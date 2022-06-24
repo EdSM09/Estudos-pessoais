1f0188633803149150b1354304a62d45cb5f39ea
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 08:37:41 -0300

    pirâmideDireita

--- Python/pirâmides.py
+++ Python/pirâmides.py
@@ -1,17 +1,19 @@
 # programa que gera diferentes modelos de piramide dependendo do input do usuário
 
 # pedir a altura da pirâmide
-
+altura = int(input("Altura da Pirâmide: "))
 # pedir o tipo da pirâmide
+tipo = input("Tipo: ")
 
 # definir as funções de cada piramide
-    # pirâmide completa ponta ara cima
+# pirâmide completa ponta ara cima
 
-    # pirâmide completa ponta para baixo
+# pirâmide completa ponta para baixo
 
-    # meia pirâmide direita
-
-    # meia pirâmide esqueda
+# meia pirâmide direita
+def pirâmideDireita(altura):
+    
+# meia pirâmide esqueda
     
 # criar os if elif statements para checar os tipos
     # passar as funções com a altura como argumento
