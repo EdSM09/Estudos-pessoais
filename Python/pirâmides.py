d8f217b27d9e9aa30ebe4f6ab5f3041d9d475579
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 16:40:39 -0300

    debugging

--- Python/pirâmides.py
+++ Python/pirâmides.py
@@ -19,9 +19,9 @@ def piramideCima(altura):
 # pirâmide completa ponta para baixo
 def piramideBaixo(altura):
     for i in range(altura, 0, -1): 
-        for j in range(2 * i + 1):
+        for j in range(altura - 1):
             print(".", end=" ")
-        for k in range(altura- i):
+        for k in range(2 * i - 1):
             print("#", end=" ")
         print( )
 # meia pirâmide direita
