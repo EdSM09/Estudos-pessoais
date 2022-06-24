b64bc82c5518a8f432ae71234bffc61b85939067
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 18:03:50 -0300

    tentar de outro jeito

--- Python/grafSegGrau.py
+++ Python/grafSegGrau.py
@@ -1,14 +1,3 @@
 # importar biblioteca
 import matplotlib.pyplot as plt
 
-x = list(range(0, 1000))
-y = x ** 2
-plt.plot(n)
-
-# dar nomes aos eixos
-plt.xlabel("Eixo X")
-plt.ylabel("Eixo Y")
-
-# dar nome ao grafico
-plt.title("Curva")
-
