03fa15bb1ae081379b1dd5e6e6f8eaadbc382cd3
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 08:24:04 -0300

    tipos de pirâmides

--- Python/pirâmides.py
+++ Python/pirâmides.py
@@ -1,1 +1,9 @@
 # programa que gera diferentes modelos de piramide dependendo do input do usuário
+
+# pirâmide completa ponta ara cima
+
+# pirâmide completa ponta para baixo
+
+# meia pirâmide direita
+
+# meia pirâmide esqueda
