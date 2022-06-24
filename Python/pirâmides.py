9976d196244cc6ac44ecdcb35274518ae476c492
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 08:28:12 -0300

    pseudoCode

--- Python/pirâmides.py
+++ Python/pirâmides.py
@@ -1,9 +1,16 @@
 # programa que gera diferentes modelos de piramide dependendo do input do usuário
 
-# pirâmide completa ponta ara cima
+# pedir a altura da pirâmide
+# pedir o tipo da pirâmide
 
-# pirâmide completa ponta para baixo
+# definir as funções de cada piramide
+    # pirâmide completa ponta ara cima
 
-# meia pirâmide direita
+    # pirâmide completa ponta para baixo
 
-# meia pirâmide esqueda
+    # meia pirâmide direita
+
+    # meia pirâmide esqueda
+    
+# criar os if elif statements para checar os tipos
+    # passar as funções com a altura como argumento
