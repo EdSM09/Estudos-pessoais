1d6c3462b43b04e5d07c65136c5f2ccd89a1ff7c
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 18:15:13 -0300

    função quadratica

--- Python/grafSegGrau.py
+++ Python/grafSegGrau.py
@@ -1,3 +1,16 @@
 # importar biblioteca
 import matplotlib.pyplot as plt
+import numpy as np
 
+x = np.arange(-100,1000,1)
+
+y = x ** 2 
+
+ptl.plot(x, y)
+
+# dar nomes aos eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+# dar nome ao grafico
+plt.title("Gráfico")
