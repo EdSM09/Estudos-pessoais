7c50fa15b308064c4b3cbcf4cb4083e4fd0ab56b
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 17:38:02 -0300

    comentários

--- Python/graficos.py
+++ Python/graficos.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 # valores de X
 x = [1,5,6,7,8,10,12]
 # valores de Y
-y = [ 5,6,7,2,38,9,7]
+y = [5,6,7,2,38,9,7]
 
+# pedir para relacionar os valores de x e y e colocar no grafico
 plt.plot(x, y)
 
+# dar nomes aos eixos
 plt.xlabel("Eixo X")
 plt.ylabel("Eixo Y")
+
+# 
