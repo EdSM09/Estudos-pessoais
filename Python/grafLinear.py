3adfb324ddc303305c97a3b96701ab4d8aa39344
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-25 09:01:11 -0300

    nomes eixo + titulo + mostrar

--- Python/grafLinear.py
+++ Python/grafLinear.py
@@ -7,5 +7,14 @@ x = [1,2,3,4,5,6,7,8,9,10]
 y = [1,2,3,4,5,6,7,8,9,10]
 
 # gerar o gráfico
+plt.plot(x,y)
 
 # dar nome aos eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+# nome do gráfico
+plt.title("Gráfico Função Linear")
+
+# mostrar o gráfico
+plt.show()
