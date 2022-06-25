d433168c6c1f3b234d1e551484d451c7429b87a9
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 22:59:36 -0300

    numpy

--- Python/grafCirculo.py
+++ Python/grafCirculo.py
@@ -1,9 +1,13 @@
 # programa que imprimi um grafico de um circulo
 # inplementar a biblioteca
 import matplotlib.pyplot as plt 
+import numpy as np
+
 
 
 # tentar alguns metôdos da internet
+
+# metodo 1
 figure, axes = plt.subplots()
 cc = plt.Circle((0.5 , 0.5), 0.4)
 
@@ -11,3 +15,5 @@ axes.set_aspect(1)
 axes.add_artist(cc)
 plt.title('Colored Circle')
 plt.show()
+
+# metodo 2
