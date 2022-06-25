6b25fd2c287779e2ad60632b08acc9bfeb017ccc
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 23:09:48 -0300

    metodo 2

--- Python/grafCirculo.py
+++ Python/grafCirculo.py
@@ -13,7 +13,20 @@ cc = plt.Circle((0.5 , 0.5), 0.4)
 
 axes.set_aspect(1)
 axes.add_artist(cc)
-plt.title('Colored Circle')
+plt.title("Circulo Colorido")
 plt.show()
 
 # metodo 2
+angulo  = np.linspace(0 , 2 * np.pi , 150)
+
+raio = 0.4
+
+x =  raio * np.cos(angulo)
+y = raio * np.sin(angulo)
+
+figure , axes = plt.subplots(1)
+
+axes.plot(x, y)
+axes.set_aspect(1)
+
+plt.title("Circuferência")
