fbe77e7d8cc3b6370b49bd66d3a4ade6c0b9a318
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-24 23:37:37 -0300

    turtle

---
+++ Python/testeTurtle.py
@@ -0,0 +1,9 @@
+# teste da biblioteca Turtle
+# ela basicamente permite que com comandos básicos voê faça desenhos simples
+
+# importar a biblioteca
+import turtle
+
+# inicializar a tela
+s = turtle.getscreen()
+
