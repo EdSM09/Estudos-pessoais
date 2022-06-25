16583c3ebae5d1d941fbf1d0519d175bf8f33f33
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-25 08:58:42 -0300

    valores + comentários

---
+++ Python/grafLinear.py
@@ -0,0 +1,11 @@
+# importar bibliotecas
+import matplotlib.pyplot as plt
+
+
+# definir os intervalos dos valores de x e y
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [1,2,3,4,5,6,7,8,9,10]
+
+# gerar o gráfico
+
+# dar nome aos eixos
