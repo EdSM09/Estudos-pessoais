a87b9d367fbf706a1debf1ccefa9ace112832eb0
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 12:09:00 -0300

    colocar tudo dentro de um loop

--- Python/PedraPapelTessoura.py
+++ Python/PedraPapelTessoura.py
@@ -5,40 +5,42 @@ import random
 # opções de escolha do jogo
 opções = ["pedra", "papel", "tessoura"]
 
-# pedir a escolha do jogador em minusculo 
-# garantir que o input é uma das opções do jogo
-jogador = None
-while jogador not in opções:
-    jogador = input("Pedra papel ou tessoura?: ").lower()
-    print(jogador)
+# fazer um loop while para repetir o jogo
+while True:
+    # pedir a escolha do jogador em minusculo 
+    # garantir que o input é uma das opções do jogo
+    jogador = None
+    while jogador not in opções:
+        jogador = input("Pedra papel ou tessoura?: ").lower()
+        print(jogador)
 
-# fazer o computador escolher um valor dentro da variávell opções 
-computador =  random.choice(opções)
-print(computador)
+    # fazer o computador escolher um valor dentro da variávell opções 
+    computador =  random.choice(opções)
+    print(computador)
 
 
-# criar os if statements de acordo com as regras do jogo 
-# empate
-if jogador == computador:
-    print("Empate! ")
-# pedra
-elif jogador == "pedra":
-    if computador == "tessoura":
-        print("Você ganhou!!!!!")
-    if computador == "papel":
-        print("Você perdeu!")
-# papel 
-elif jogador == "papel":
-    if computador == "pedra":
-        print("Você ganhou!!!!!")
-    if computador == "tessoura":
-        print("Você perdeu!")
+    # criar os if statements de acordo com as regras do jogo 
+    # empate
+    if jogador == computador:
+        print("Empate! ")
+    # pedra
+    elif jogador == "pedra":
+        if computador == "tessoura":
+            print("Você ganhou!!!!!")
+        if computador == "papel":
+            print("Você perdeu!")
+    # papel 
+    elif jogador == "papel":
+        if computador == "pedra":
+            print("Você ganhou!!!!!")
+        if computador == "tessoura":
+            print("Você perdeu!")
 
-# tessoura
-elif jogador == "tessoura":
-    if computador == "papel":
-        print("Você ganhou!!!!!")
-    if computador == "pedra":        
-        print("Você perdeu!")
+    # tessoura
+    elif jogador == "tessoura":
+        if computador == "papel":
+            print("Você ganhou!!!!!")
+        if computador == "pedra":        
+            print("Você perdeu!")
 
         
