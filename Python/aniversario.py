fa407f96b9dfbbdf5497ab3db58503b316f51429
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 09:32:32 -0300

    comentários

--- Python/aniversario.py
+++ Python/aniversario.py
@@ -16,11 +16,11 @@ aniversarioS = aniversario.strftime("%d/%m/%y")
 # calcular a diferença em dias entre as datas 
 def difDatas(dataAtual, aniversario):
     return (aniversario - dataAtual).days
+
 # comparar a string que representa a data atual com data do meu aniversário
 # imprimir se é ou não é meu aniversário
-
-if dataAtualS == aniversarioS:
-    print("Parabéns é seu aniversário!!!!")
-else:
-    print("Poxa, não é seu aniversário")
-    print("Mas faltam ", difDatas(dataAtual, aniversario), "Dias")
+if dataAtualS == aniversarioS: # se as datas forem iguais imprima 
+    print("Parabéns é seu aniversário!!!!") 
+else: # se elas não forem
+    print("Poxa, não é seu aniversário") # imprimima a mensagem "Poxa, não é seu aniversário"
+    print("Mas faltam ", difDatas(dataAtual, aniversario), "dias") # e imprima a diferença entre as datas 
