a07bc0febbbfbaab1726002a9f05fd7be1aee334
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 11:37:18 -0300

    pedir o input do jogador

--- Python/PedraPapelTessoura.py
+++ Python/PedraPapelTessoura.py
@@ -5,7 +5,12 @@ import random
 # opções de escolha do jogo
 opções = ["pedra", "papel", "tessoura"]
 
+# pedir a escolha do jogador em minusculo 
+jogador = input("Pedra papel ou tessoura?: ").lower()
+print(jogador)
 # fazer o computador escolher um valor dentro da variávell opções 
 computador =  random.choice(opções)
 print(computador)
+
+
 
