cac9149d4745e9678bac660fc1b4caf7dbcbeeec
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 10:06:32 -0300

    lista

--- Python/sumOfInts.py
+++ Python/sumOfInts.py
@@ -1,6 +1,6 @@
 # programa que soma todas as ints dentro de um intervalo finito
 
 # criar uma lista de numeros 
-# definir a função que faz a soma
-
+num = list(0,100)
+print(num)
 # passar a função com a lista como argumento 
