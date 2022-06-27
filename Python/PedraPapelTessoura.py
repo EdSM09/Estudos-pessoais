29393cc573416910cbe18f0df38d898cb5297363
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 11:33:57 -0300

    randomizar as escolhas do computador

--- Python/PedraPapelTessoura.py
+++ Python/PedraPapelTessoura.py
@@ -5,3 +5,7 @@ import random
 # opções de escolha do jogo
 opções = ["pedra", "papel", "tessoura"]
 
+# fazer o computador escolher um valor dentro da variávell opções 
+computador =  opções.choice()
+print(computador)
+
