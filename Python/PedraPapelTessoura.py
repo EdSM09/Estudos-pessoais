cdddad1cb6e65f7b0fccbf4ff7c8b73cfc55f6e1
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 11:40:22 -0300

    syntaxError

--- Python/PedraPapelTessoura.py
+++ Python/PedraPapelTessoura.py
@@ -8,7 +8,7 @@ opções = ["pedra", "papel", "tessoura"]
 # pedir a escolha do jogador em minusculo 
 # garantir que o input é uma das opções do jogo
 jogador = none
-while jogador not in opções
+while jogador not in opções:
     jogador = input("Pedra papel ou tessoura?: ").lower()
 print(jogador)
 
