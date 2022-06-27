3dc5d5e9e57c8f2d05efe5b73fbb6fff1331eba0
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 08:45:52 -0300

    pseudocode

---
+++ Python/aniversario.py
@@ -0,0 +1,12 @@
+# programa que checa a data e confirm se é meu aniversário 
+# ele vai servir como um teste da biblioteca datetime 
+# importar a biblioteca
+
+# criar uma variável que irá armazenar a data atual
+
+# transformar essa data em uma string usando strftime()
+
+# comparar a string que representa a data atual com data do meu aniversário
+
+# imprimir se é ou não é meu aniversário
+# calcular a diferença em dias entre as datas 
