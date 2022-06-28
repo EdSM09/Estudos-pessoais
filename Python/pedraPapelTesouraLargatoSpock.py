9499dd8c2d2ece4585dbc2796bb0a4cf06b2d822
EdSM09 <98329754+EdSM09@users.noreply.github.com>
2022-06-27 22:41:50 -0300

    jogo de base mesmo jogo que eu ja tinha feito

---
+++ Python/pedraPapelTesouraLargatoSpock.py
@@ -0,0 +1,61 @@
+# jogo básico de pedra papel e tesoura 
+# importar a biblioteca random
+import random 
+
+# opções de escolha do jogo
+opções = ["pedra", "papel", "tesoura"]
+
+# fazer um loop while para repetir o jogo
+while True:
+    # pedir a escolha do jogador em minusculo 
+    # garantir que o input é uma das opções do jogo
+    jogador = None
+    while jogador not in opções:
+        jogador = input("Pedra papel ou tesoura?: ").lower()
+
+    # fazer o computador escolher um valor dentro da variávell opções 
+    computador =  random.choice(opções)
+
+
+    # criar os if statements de acordo com as regras do jogo 
+    # empate
+    if jogador == computador:
+        print("Jogador: ",jogador)
+        print("Computador: ",computador)
+        print("Empate! ")
+    # pedra
+    elif jogador == "pedra":
+        if computador == "tesoura":
+            print("Jogador: ",jogador)
+            print("Computador: ",computador)
+            print("Você ganhou!!!!!")
+        if computador == "papel":
+            print("Jogador: ",jogador)
+            print("Computador: ",computador)
+            print("Você perdeu!")
+    # papel 
+    elif jogador == "papel":
+        if computador == "pedra":
+            print("Jogador: ",jogador)
+            print("Computador: ",computador)
+            print("Você ganhou!!!!!")
+        if computador == "tesoura":
+            print("Jogador: ",jogador)
+            print("Computador: ",computador)
+            print("Você perdeu!")
+
+    # tesoura
+    elif jogador == "tesoura":
+        if computador == "papel":
+            print("Jogador: ",jogador)
+            print("Computador: ",computador)
+            print("Você ganhou!!!!!")
+        if computador == "pedra":   
+            print("Jogador: ",jogador)
+            print("Computador: ",computador)     
+            print("Você perdeu!")
+    jogarNovamente = input("Você que jogar de novo (sim/não)?: ").lower()
+    
+    if jogarNovamente != "sim":
+        break
+print("Tchaul!")
